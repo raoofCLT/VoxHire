d1c2fcbf7d411bfef81fa3ddf3e2008b66b74c12
Abdul Raoof <raoofkottayil@gmail.com>
2025-04-15 15:42:55 +0530

    Complete home page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,17 @@
+import Link from 'next/link'
 import React, { ReactNode } from 'react'
+import Image from 'next/image'
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div>
-      {children}
+    <div className="root-layout">
+     <nav>
+      <Link href="/" className="flex items-center gap-2">
+      <Image src="/logo.svg" alt="Logo" width={38} height={32} />
+      <h2 className="text-primary-100">VoxHire</h2>
+      </Link>
+     </nav>
+     {children}
     </div>
   )
 }
